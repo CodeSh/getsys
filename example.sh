7b9528cf8c40d05defe43bbc4a6796285500ca37
Harry <jarriztg@users.noreply.github.com>
2016-12-17 06:32:46 -0600

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
+
 action=install		# The packages will be installed.
 sudo=true		# Require sudo privileges.
+confirm=false		# Install the packages without confirm
 update_packages=true	# Finishing the installation the repositories will be updated.
 
-packages_all=( # To all systems
-  "curl"
-  "gcc"
-  )
+packages_all=( # To Debian, suseLinux, openSUSE, ArchLinux and Fedora (recommended if the package is general)
+ 	"curl"
+)
   
-packages_debian=( # In debian
-  "g++"
-  )
+packages_debian=( # On Debian, Ubuntu, etc
+	"g++"
+)
+  
+packages_archlinux=( # On Arch or derivatives
+ 	"gcc"
+)
+ 
+packages_fedora=(
+  	"jq"
+)
+
+packages_opensuse=( # On openSUSE or derivatives
+	"ssh"
+)
 
 source getsys # get the system
 
